668b24e509d8247e45611ea6d6d99d097307cb01
prateek <prateek.yadav@toptal.com>
2021-02-26 17:18:40 +0530

    meta updated

--- src/App.js
+++ src/App.js
@@ -15,21 +15,19 @@ function App({ lang }) {
 	return (
 		<section className="home h-100">
 			{lang === 'fr' ?
-				(
-					<HelmetProvider><Helmet>
-						<meta property="og:title" content="KIBU" />
-						<meta property="og:url" content="https://www.kibugame.com/fr" />
+				<HelmetProvider>
+					<Helmet>
 						<meta property="og:description" content="Kibu - Ça Reste Entre Nous®️ est un jeu social amusant qui approfondit vos relations avec vos amis, votre famille, vos 'dates', vos collègues et même des inconnus amusants. 
 						Ce jeu est truffé de questions qui permettent d'entamer et de poursuivre des conversations incroyables. En posant les bonnes questions, vous comprendrez mieux qui sont vos amis et ce qui les motive." />
 						<meta property="og:image" content="%PUBLIC_URL%/logofr.png" />
-					</Helmet></HelmetProvider>) :
-				(
-					<HelmetProvider><Helmet>
-						<meta property="og:title" content="KIBU" />
-						<meta property="og:url" content="https://www.kibugame.com/en" />
+					</Helmet>
+				</HelmetProvider> :
+				<HelmetProvider>
+					<Helmet>
 						<meta property="og:description" content="Kibu is a fun social game that deepens your relationships with friends, family, dates, colleagues, and even fun strangers. This game is filled with questions to start incredible conversations and keep them going. By asking the right questions, you’ll gain a better understanding of who your friends are and what makes them tick." />
 						<meta property="og:image" content="%PUBLIC_URL%/logoen.png" />
-					</Helmet></HelmetProvider>)
+					</Helmet>
+				</HelmetProvider>
 			}
 			<div className="container fixed-top px-0 mt-3">
 				<nav className="navbar navbar-expand-lg navbar-light">

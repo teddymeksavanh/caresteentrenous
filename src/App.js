808128ad0b476292d30e725c09199da18339f354
prateek <prateek.yadav@toptal.com>
2021-02-26 17:05:16 +0530

    updated helmet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import i18n from './i18n.json';
 import { Link } from "react-router-dom";
 import './App.scss';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 function App({ lang }) {
 	const [toggle, setToggle] = useState(false);
@@ -13,6 +14,23 @@ function App({ lang }) {
 	}
 	return (
 		<section className="home h-100">
+			{lang === 'fr' ?
+				(
+					<HelmetProvider><Helmet>
+						<meta property="og:title" content="KIBU" />
+						<meta property="og:url" content="https://www.kibugame.com/fr" />
+						<meta property="og:description" content="Kibu - Ça Reste Entre Nous®️ est un jeu social amusant qui approfondit vos relations avec vos amis, votre famille, vos 'dates', vos collègues et même des inconnus amusants. 
+						Ce jeu est truffé de questions qui permettent d'entamer et de poursuivre des conversations incroyables. En posant les bonnes questions, vous comprendrez mieux qui sont vos amis et ce qui les motive." />
+						<meta property="og:image" content="%PUBLIC_URL%/logofr.png" />
+					</Helmet></HelmetProvider>) :
+				(
+					<HelmetProvider><Helmet>
+						<meta property="og:title" content="KIBU" />
+						<meta property="og:url" content="https://www.kibugame.com/en" />
+						<meta property="og:description" content="Kibu is a fun social game that deepens your relationships with friends, family, dates, colleagues, and even fun strangers. This game is filled with questions to start incredible conversations and keep them going. By asking the right questions, you’ll gain a better understanding of who your friends are and what makes them tick." />
+						<meta property="og:image" content="%PUBLIC_URL%/logoen.png" />
+					</Helmet></HelmetProvider>)
+			}
 			<div className="container fixed-top px-0 mt-3">
 				<nav className="navbar navbar-expand-lg navbar-light">
 					<a className="navbar-brand" href="/">
@@ -40,7 +58,7 @@ function App({ lang }) {
 								<a className="empty-a" href="mailto:support@kibugame.com">Contact</a>
 							</li>
 							<li>
-								<a className="empty-a" href={lang === 'fr' ? 'https://apps.apple.com/fr/app/kibu-fun-convos-with-friends/id1543589401?l=fr' : 'https://apps.apple.com/fr/app/kibu-fun-convos-with-friends/id1543589401?l=en'} target="_blank" rel="noreferrer">{lang === 'fr'? 'Note' : 'Rate'} Kibu ⭐</a>
+								<a className="empty-a" href={lang === 'fr' ? 'https://apps.apple.com/fr/app/kibu-fun-convos-with-friends/id1543589401?l=fr' : 'https://apps.apple.com/fr/app/kibu-fun-convos-with-friends/id1543589401?l=en'} target="_blank" rel="noreferrer">{lang === 'fr' ? 'Note' : 'Rate'} Kibu ⭐</a>
 
 							</li>
 						</ul>

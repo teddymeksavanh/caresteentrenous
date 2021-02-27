1bc9d151dd4c89063d5840a9f0fb45664c8d2ac5
prateek <prateek.yadav@toptal.com>
2021-02-27 12:14:31 +0530

    removing helmet

--- src/index.js
+++ src/index.js
@@ -16,23 +16,6 @@ const lang = window.navigator.language;
 
 ReactDOM.render(
 	<>
-		{/^en\b/.test(lang) ?
-			<HelmetProvider>
-				<Helmet>
-					<title>Kibu - Fun Convos With Friends</title>
-					<meta property="og:description" content="Kibu is a fun social game that deepens your relationships with friends, family, dates, colleagues, and even fun strangers. This game is filled with questions to start incredible conversations and keep them going. By asking the right questions, you’ll gain a better understanding of who your friends are and what makes them tick." />
-					<meta property="og:image" content="./logoen.png" />
-				</Helmet>
-			</HelmetProvider> :
-			<HelmetProvider>
-				<Helmet>
-					<title>Kibu - Ça Reste Entre Nous‪®</title>
-					<meta property="og:description" content="Kibu - Ça Reste Entre Nous®️ est un jeu social amusant qui approfondit vos relations avec vos amis, votre famille, vos 'dates', vos collègues et même des inconnus amusants. 
-					Ce jeu est truffé de questions qui permettent d'entamer et de poursuivre des conversations incroyables. En posant les bonnes questions, vous comprendrez mieux qui sont vos amis et ce qui les motive." />
-					<meta property="og:image" content="./logofr.png" />
-				</Helmet>
-			</HelmetProvider>
-		}
 		<React.StrictMode>
 			<Router>
 				<Switch>

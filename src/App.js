3bf71b1821010e388322a1d694f4b111f1aec1ca
prateek <prateek.yadav@toptal.com>
2021-03-10 12:45:50 +0530

    apple istore meta added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
 				<meta name="description" content="Kibu is a fun social game that deepens your relationships with friends, family, dates, colleagues, and even fun strangers. This game is filled with thought-provoking questions to start incredible conversations and keep them going. By asking the right questions, you’ll gain a better understanding of who your friends are and what makes them tick." />
 				<meta property="og:description" content="Kibu is a fun social game that deepens your relationships with friends, family, dates, colleagues, and even fun strangers. This game is filled with thought-provoking questions to start incredible conversations and keep them going. By asking the right questions, you’ll gain a better understanding of who your friends are and what makes them tick." />
 				<meta property="og:image" content="https://www.kibugame.com/en.png" />
+				<meta name="apple-itunes-app" content="app-id=1543589401, app-argument='https://apps.apple.com/fr/app/kibu-fun-convos-with-friends/id1543589401?l=en'" />
 			</Helmet>
 			<Homepage lang="en" />
 		</>
--- src/Appfr.js
+++ src/Appfr.js
@@ -14,6 +14,7 @@ function AppFR() {
                 <meta property="og:description" content="Kibu - Ça Reste Entre Nous®️ est un jeu social amusant qui approfondit vos relations avec vos amis, votre famille, vos 'dates', vos collègues et même des inconnus amusants. 
                     Ce jeu est truffé de questions qui permettent d'entamer et de poursuivre des conversations incroyables. En posant les bonnes questions, vous comprendrez mieux qui sont vos amis et ce qui les motive." />
                 <meta property="og:image" content="https://www.kibugame.com/fr.png" />
+				<meta name="apple-itunes-app" content="app-id=1543589401, app-argument='https://apps.apple.com/fr/app/kibu-fun-convos-with-friends/id1543589401?l=fr'" />
             </Helmet>
 			<Homepage lang="fr" />
 		</>
